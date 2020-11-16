4e83511266c7382315e063089ec025969526c315
pmi4202 <pmi4202@naver.com>
2020-11-16 17:01:32 +0900

    캘린더

--- HaruJogak/app/src/main/java/com/example/harujogak/Calendar.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/Calendar.java
@@ -1,7 +1,6 @@
 package com.example.harujogak;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
@@ -10,11 +9,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Calendar extends AppCompatActivity {
     //Search(Date)
     //addScheduleByDate
     //Compute Day Static
     //Compute Week Static
+    long mNow;
+    Date mDate;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy / MM / dd");
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar);
@@ -24,17 +30,22 @@ public class Calendar extends AppCompatActivity {
         EditText calendar_text = findViewById(R.id.calendar_text);
         Button calendar_btn = findViewById(R.id.calendar_btn);
 
+        calendar_date.setText(getDate());
+        calendar_text.setText("");
+
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
-                calendar_date.setVisibility(View.VISIBLE);
-                calendar_text.setVisibility(View.VISIBLE);
-                calendar_btn.setVisibility(View.VISIBLE);
-
                 calendar_date.setText(String.format("%d / %d / %d",year,month+1,dayOfMonth));
                 calendar_text.setText("");
             }
         });
 
+    }
+
+    private String getDate(){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return dateFormat.format(mDate);
     }
 
 }
