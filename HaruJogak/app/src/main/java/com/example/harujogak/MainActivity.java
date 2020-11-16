6c6c69ace7f958404c7ddc0594d17392427aa919
pmi4202 <pmi4202@naver.com>
2020-11-16 17:01:07 +0900

    메인페이지, 캘린더 -현재시간/날짜

--- HaruJogak/app/src/main/java/com/example/harujogak/MainActivity.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/MainActivity.java
@@ -5,16 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     Button btn1, btn2, btn3, btn4, btn5;
+    TextView date, time;
+    long mNow;
+    Date mDate;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년MM월dd일");
+    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        date = findViewById(R.id.main_date);
+        time = findViewById(R.id.main_time);
+
         btn1 = (Button) findViewById(R.id.main_btn1);
         btn2 = (Button) findViewById(R.id.main_btn2);
         btn3 = (Button) findViewById(R.id.main_btn3);
@@ -26,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         btn3.setOnClickListener(listener);
         btn4.setOnClickListener(listener);
         btn5.setOnClickListener(listener);
+
+        date.setText(getDate());
+        time.setText(getTime());
     }
 
     class Listener implements View.OnClickListener{
@@ -52,4 +68,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     Listener listener = new Listener();
+
+    private String getDate(){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return dateFormat.format(mDate);
+    }
+    private String getTime(){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return timeFormat.format(mDate);
+    }
 }
