0c9142a3dca108fd17f17dc1e25a118dec600702
pmi4202 <pmi4202@naver.com>
2020-11-16 18:58:46 +0900

    클래스이름 수정

--- HaruJogak/app/src/main/java/com/example/harujogak/Goal.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/Goal.java
@@ -1,38 +1,10 @@
 package com.example.harujogak;
 
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Goal extends AppCompatActivity {
+public class Goal {
     String goal_name;
     int Deadline;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.goal);
 
-        CalendarView goal_calendar = findViewById(R.id.goal_calendar);
-        TextView goal_text = findViewById(R.id.goal_text);
-        EditText goal_input = findViewById(R.id.goal_input);
-        TextView goal_result = findViewById(R.id.goal_result);
-        Button goal_btn = findViewById(R.id.calendar_btn);
-
-        goal_text.setText("나의 목표는");
-        goal_result.setText("D-day");
-
-        goal_calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
-
-            }
-        });
-    }
 
     public String computeGoalStatic(){
         String result="결과 값 문자열";
