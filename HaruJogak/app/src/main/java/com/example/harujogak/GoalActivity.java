2706b33dc8442533c68d9d0a0e0b78c94edbfe21
pmi4202 <pmi4202@naver.com>
2020-11-20 22:11:34 +0900

    목표 리스트 페이지, 디데이 계산

--- HaruJogak/app/src/main/java/com/example/harujogak/GoalActivity.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/GoalActivity.java
@@ -1,32 +1,24 @@
 package com.example.harujogak;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GoalActivity extends AppCompatActivity {
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.goal);
 
-        CalendarView goal_calendar = findViewById(R.id.goal_calendar);
-        TextView goal_text = findViewById(R.id.goal_text);
-        EditText goal_input = findViewById(R.id.goal_input);
-        TextView goal_result = findViewById(R.id.goal_result);
-        Button goal_btn = findViewById(R.id.calendar_btn);
-
-        goal_text.setText("나의 목표는");
-        goal_result.setText("D-day");
-
-        goal_calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
-
+        btn=(Button) findViewById(R.id.goal_btn);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(GoalActivity.this, GoalAddActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ HaruJogak/app/src/main/java/com/example/harujogak/GoalAddActivity.java
@@ -0,0 +1,59 @@
+package com.example.harujogak;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class GoalAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goal_add);
+
+        CalendarView goal_calendar = findViewById(R.id.goal_add_calendar);
+        TextView goal_text = findViewById(R.id.goal_add_text);
+        EditText goal_input = findViewById(R.id.goal_add_input);
+        TextView goal_result = findViewById(R.id.goal_add_result);
+        Button goal_btn = findViewById(R.id.calendar_btn);
+
+        goal_text.setText("나의 목표는");
+        goal_result.setText("D-day");
+
+        goal_calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
+                int tYear, tMonth, tDay;
+                long t, d;
+                int dday;
+
+                Calendar tcalendar = Calendar.getInstance();
+                Calendar dcalendar = Calendar.getInstance();
+                //오늘 날짜
+                tYear = tcalendar.get(Calendar.YEAR);
+                tMonth = tcalendar.get(Calendar.MONTH);
+                tDay = tcalendar.get(Calendar.DAY_OF_MONTH);
+                //목표 날짜
+                dcalendar.set(year, month, dayOfMonth);
+                //날짜 초단위로 변경
+                t=tcalendar.getTimeInMillis()/(24*60*60*1000);
+                d=dcalendar.getTimeInMillis()/(24*60*60*1000);
+                dday=(int)(d-t);
+                if(dday>0)
+                    goal_result.setText("D+"+Integer.toString(dday));
+                else if(dday==0)
+                    goal_result.setText("D-day");
+                else
+                    goal_result.setText("D"+Integer.toString(dday));
+            }
+        });
+    }
+
+
+}
