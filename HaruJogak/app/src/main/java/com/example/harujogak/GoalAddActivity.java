ab0a0ab9a1811b2df803c3c052e37a3ac225acad
이현지 <leehj0483@gmail.com>
2020-12-01 02:36:09 +0900

    큰 목표, 날짜 지정 목표 firebase 연동

--- HaruJogak/app/src/main/java/com/example/harujogak/GoalAddActivity.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/GoalAddActivity.java
@@ -1,6 +1,7 @@
 package com.example.harujogak;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
@@ -9,20 +10,29 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Calendar;
 
 public class GoalAddActivity extends AppCompatActivity {
-
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.goal_add);
+
+        //firebase 정의
+        database = FirebaseDatabase.getInstance();
+        myRef=database.getReference();
 
         CalendarView goal_calendar = findViewById(R.id.goal_add_calendar);
         TextView goal_text = findViewById(R.id.goal_add_text);
         EditText goal_input = findViewById(R.id.goal_add_input);
         TextView goal_result = findViewById(R.id.goal_add_result);
         Button goal_btn = findViewById(R.id.calendar_btn);
+        Button goal_add_btn=findViewById(R.id.goal_add_btn);
 
         goal_text.setText("나의 목표는");
         goal_result.setText("D-day");
@@ -35,12 +45,16 @@ public class GoalAddActivity extends AppCompatActivity {
 
                 Calendar tcalendar = Calendar.getInstance();
                 Calendar dcalendar = Calendar.getInstance();
+
                 //오늘 날짜
                 tYear = tcalendar.get(Calendar.YEAR);
                 tMonth = tcalendar.get(Calendar.MONTH);
                 tDay = tcalendar.get(Calendar.DAY_OF_MONTH);
+
                 //목표 날짜
                 dcalendar.set(year, month, dayOfMonth);
+                myRef.child("UserID").child("목표D-day").setValue(Integer.toString(year)+"년 "+Integer.toString(month)+"월 "+Integer.toString(dayOfMonth)+"일");
+
                 //날짜 초단위로 변경
                 t=tcalendar.getTimeInMillis()/(24*60*60*1000);
                 d=dcalendar.getTimeInMillis()/(24*60*60*1000);
@@ -51,6 +65,15 @@ public class GoalAddActivity extends AppCompatActivity {
                     goal_result.setText("D-day");
                 else
                     goal_result.setText("D"+Integer.toString(dday));
+            }
+        });
+
+        goal_add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getGoal =goal_input.getText().toString();
+                myRef.child("UserID").child("목표").setValue(getGoal);
+                goal_input.setText("");
             }
         });
     }
--- HaruJogak/app/src/main/java/com/example/harujogak/ScheduleActivity.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/ScheduleActivity.java
@@ -1,6 +1,7 @@
 package com.example.harujogak;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
@@ -9,17 +10,29 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class ScheduleActivity extends AppCompatActivity {
     long mNow;
     Date mDate;
+    char[] fb_date =new char[10]; //firebase에 키 값으로 들어갈 날짜
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy / MM / dd");
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar);
+
+        //firebase 정의
+        database=FirebaseDatabase.getInstance();
+        myRef=database.getReference();
 
         CalendarView calendar = findViewById(R.id.calendar);
         TextView calendar_date = findViewById(R.id.calender_date);
@@ -32,10 +45,21 @@ public class ScheduleActivity extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth){
                 calendar_date.setText(String.format("%d / %d / %d",year,month+1,dayOfMonth));
+                String date=Integer.toString(year)+"년 "+Integer.toString(month+1)+"월 "+Integer.toString(dayOfMonth)+"일";
+                fb_date=null;
+                fb_date=date.toCharArray();
                 calendar_text.setText("");
             }
         });
-
+        calendar_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input_date=String.valueOf(fb_date);
+                String getDayGoal=calendar_text.getText().toString();
+                calendar_text.setText("");
+                myRef.child("UserID").child(input_date).child(getDayGoal).setValue("");
+            }
+        });
     }
 
     private String getDate(){
