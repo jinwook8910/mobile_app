7162383f413d425f73c1cfec7cfbeb17342a6878
pmi4202 <pmi4202@naver.com>
2020-12-03 18:55:00 +0900

    실시간 날짜, 시간

--- HaruJogak/app/src/main/java/com/example/harujogak/MainActivity.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.harujogak;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -45,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
         btn3.setOnClickListener(listener);
         btn4.setOnClickListener(listener);
 
-        date.setText(getDate());
-        time.setText(getTime());
+        //date.setText(getDate());
+        //time.setText(getTime());
+        ShowTimeMethod();
 
         PieChart pieChart = findViewById(R.id.todayPieChart);
         pieChart.setUsePercentValues(false);
@@ -117,5 +120,27 @@ public class MainActivity extends AppCompatActivity {
         mNow = System.currentTimeMillis();
         mDate = new Date(mNow);
         return timeFormat.format(mDate);
+    }
+    public void ShowTimeMethod(){
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                date.setText(getDate());
+                time.setText(getTime());
+            }
+        };
+        Runnable task = new Runnable(){
+            @Override
+            public void run(){
+                while(true){
+                    try{
+                        Thread.sleep(1000);
+                    }catch(InterruptedException e){}
+                    handler.sendEmptyMessage(1);//핸들러 호출
+                }
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
     }
 }
