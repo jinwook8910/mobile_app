3a5ec4789da39fd05a30319ea0e0de0207894893
이현지 <leehj0483@gmail.com>
2020-12-14 01:41:20 +0900

    통계 수정, 목표 통계

--- HaruJogak/app/src/main/java/com/example/harujogak/GoalActivity.java
+++ HaruJogak/app/src/main/java/com/example/harujogak/GoalActivity.java
@@ -2,24 +2,37 @@ package com.example.harujogak;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class GoalActivity extends AppCompatActivity {
     Button btn;
-    ArrayList<String> goal_list = new ArrayList<>();
+    ArrayList<String> goal_list=new ArrayList<>();
+    MainActivity main=new MainActivity();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.goal);
-
+        goal_list=MainActivity.getGoal_list();
 //        //목표 리스트 출력
 //        System.out.println("목표리스트 출력");
 //        User user = User.getInstance();
@@ -29,14 +42,8 @@ public class GoalActivity extends AppCompatActivity {
 //            goal_list.add(goal.getGoal_name() + goal.getDeadline());
 //            System.out.println(goal.getGoal_name() + goal.getDeadline());
 //        }
-
-        //test data
-        ArrayList<String> goal_list1 = new ArrayList<>();
-        goal_list1.add("hello1");
-        goal_list1.add("hello2");
-
         //listview
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, goal_list1);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, goal_list);
         ListView listview = (ListView) findViewById(R.id.goal_list);
         listview.setAdapter(adapter);
 
@@ -44,6 +51,7 @@ public class GoalActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView parent, View v, int position, long id){
                 //listview 객체 클릭할 때 이벤트
+
             }
         });
 
@@ -56,5 +64,4 @@ public class GoalActivity extends AppCompatActivity {
             }
         });
     }
-
 }
